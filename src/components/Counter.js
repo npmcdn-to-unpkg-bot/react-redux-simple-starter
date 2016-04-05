716d3511e02cd809c00d84e1b1cfe42244255649
Lauri Laaksonen <lauri@laaksonen.io>
2016-04-05 15:19:32 +0300

    Organize package.json & install nodemon

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,9 +5,9 @@ export default class Counter extends Component {
     const { counter, increment, decrement } = this.props;
     return (
       <div>
-        <div className="btn-group m-b-1" role="group">
-          <button type="button" className="btn btn-secondary" onClick={decrement}>-</button>
-          <button type="button" className="btn btn-secondary" onClick={increment}>+</button>
+        <div>
+          <button type="button" onClick={decrement}>-</button>
+          <button type="button" onClick={increment}>+</button>
         </div>
         <p>Count: {counter}</p>
       </div>
