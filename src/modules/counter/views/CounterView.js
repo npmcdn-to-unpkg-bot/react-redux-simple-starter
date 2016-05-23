a8a5b8cbb23574190cfdc16b5e456f74f1d24a24
Lauri Laaksonen <lauri@laaksonen.io>
2016-05-24 02:56:16 +0300

    Add NotFoundView and react-helmet

--- src/modules/counter/views/CounterView.js
+++ src/modules/counter/views/CounterView.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Helmet from 'react-helmet';
 import { actions } from '../counter';
 import Counter from '../components/Counter';
 
@@ -10,6 +11,7 @@ class CounterView extends Component {
 
     return (
       <div>
+        <Helmet title="Counter" />
         <Counter counter={counter} increment={increment} decrement={decrement} />
       </div>
     );
--- src/modules/landing/views/LandingView.js
+++ src/modules/landing/views/LandingView.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Helmet from 'react-helmet';
 
 export default class CounterView extends Component {
 
   render() {
     return (
       <div>
+        <Helmet title="simple-starter" />
         <h1>Hello from home</h1>
       </div>
     );
